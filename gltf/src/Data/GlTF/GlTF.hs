78b10592b174cf4cea8de6d3777542fca06a2e8d
as-capabl <as.capabl@gmail.com>
2020-02-15 15:47:51 +0900

    Refactoring

--- gltf/src/Data/GlTF/GlTF.hs
+++ gltf/src/Data/GlTF/GlTF.hs
@@ -24,34 +24,38 @@ import Data.GlTF.Prim
 
 do
     scRoot <- pathRelativeToCabalPackage "schema"
-    let onMember_custom _ (CONJCT.getModuleSummary -> CONJCT.ModuleSummary{..}) mn _
-            | msModuleName == "sampler.schema.json" =
-                if
-                    | mn == "magFilter" || mn == "minFilter" || mn == "wrapS" || mn == "wrapT" -> Just $
-                      do
-                        nMem <- maybe (fail "memberName") return $ CONJCT.memberNameDefault msModuleName mn
-                        return $ CONJCT.mkFieldInfo nMem (ConT ''Maybe `AppT` ConT ''Int32) mn '(J..:)
-                    | otherwise -> Nothing
-            | msModuleName == "accessor.schema.json" =
-                if
-                    | mn == "componentType" -> Just $ 
-                      do
-                        nMem <- maybe (fail "memberName") return $ CONJCT.memberNameDefault msModuleName mn
-                        return $ CONJCT.mkFieldInfo nMem (ConT ''Word32) mn '(J..:)
-                    | mn == "type" -> Just $
-                      do
-                        nMem <- maybe (fail "memberName") return $ CONJCT.memberNameDefault msModuleName mn
-                        return $ CONJCT.mkFieldInfo nMem (ConT ''AccType) mn '(J..:)
-                    | otherwise -> Nothing
-            | otherwise = Nothing
-        onType_custom _ o =
+    let onMember_sampler _ (CONJCT.getModuleSummary -> CONJCT.ModuleSummary{..}) mn _ =
+          do
+            guard $ msModuleName == "sampler.schema.json"
+            if
+                | mn == "magFilter" || mn == "minFilter" || mn == "wrapS" || mn == "wrapT" -> Just $
+                  do
+                    nMem <- maybe (fail "memberName") return $ CONJCT.memberNameDefault msModuleName mn
+                    return $ CONJCT.mkFieldInfo nMem (ConT ''Maybe `AppT` ConT ''Int32) mn '(J..:)
+                | otherwise -> Nothing
+
+        onMember_accessor _ (CONJCT.getModuleSummary -> CONJCT.ModuleSummary{..}) mn _ =
+          do
+            guard $ msModuleName == "accessor.schema.json"
+            if
+                | mn == "componentType" -> Just $ 
+                  do
+                    nMem <- maybe (fail "memberName") return $ CONJCT.memberNameDefault msModuleName mn
+                    return $ CONJCT.mkFieldInfo nMem (ConT ''Word32) mn '(J..:)
+                | mn == "type" -> Just $
+                  do
+                    nMem <- maybe (fail "memberName") return $ CONJCT.memberNameDefault msModuleName mn
+                    return $ CONJCT.mkFieldInfo nMem (ConT ''AccType) mn '(J..:)
+                | otherwise -> Nothing
+    
+        onType_float _ o =
           do
             guard $ o `CONJCT.isType` "number"
             return $ return $ ConT ''Float
 
         stgDef = CONJCT.defaultSchemaSetting (T.pack scRoot)
-        onM = onMember_custom : CONJCT.onMember stgDef
-        onT = onType_custom : CONJCT.onType stgDef
+        onM = onMember_sampler : onMember_accessor : CONJCT.onMember stgDef
+        onT = onType_float : CONJCT.onType stgDef
         stg = CONJCT.SimpleSchemaSetting $
             stgDef {
                 CONJCT.onMember = onM,
